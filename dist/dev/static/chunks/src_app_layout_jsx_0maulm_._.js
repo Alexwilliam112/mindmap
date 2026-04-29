06e83ed17757ac2b5eed97620c205ce5a221ea79
Alexander William <alexwilliam112@gmail.com>
2026-04-29 20:45:01 +0700

    initial commit

--- dist/dev/static/chunks/src_app_layout_jsx_0maulm_._.js
+++
@@ -1,5 +0,0 @@
-(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: ["static/chunks/[root-of-the-server]__0lfpob6._.css","static/chunks/0rlg_next_dist_0j9kegu._.js"],
-    source: "dynamic"
-});
